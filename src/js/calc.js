882bb295a9c51b88deed68107870f34f8f1df805
Alex Thomas <aletho24@gmail.com>
2016-12-13 00:50:02 -0500

    src/js/calc.js

--- src/js/calc.js
+++ src/js/calc.js
@@ -1,47 +1,50 @@
-(function() {
-    'use strict';
+// (function() {
+//     'use strict';
+//
+//     window.calc = window.calc || {};
+//
+//     window.calc.sum = sum;
 
-    window.calc = window.calc || {};
+/**
+ * Adds whatever numbers are provided and returns the total
+ * @param  {Array} numbers  The numbers to add together
+ * @return {Number}         The total of all numbers
+ */
+module.exports.sum = function sum(numbers) {
+    var total = 0;
 
-    window.calc.sum = sum;
-
-    /**
-     * Adds whatever numbers are provided and returns the total
-     * @param  {Array} numbers  The numbers to add together
-     * @return {Number}         The total of all numbers
-     */
-    function sum(numbers) {
-        var total = 0;
-
-        if (!numbers) {
-            return total;
-        }
-
-        numbers.forEach(function addThem(num) {
-            total += num;
-        });
-    }
-
-    /**
-     * Returns the factorial for the given number which is defined as:
-     *    1 * 2 * 3 * ... * topNumber
-     *    For example, if the number is 5, the factorial would be:
-     *    1 * 2 * 3 * 4 * 5 = 120
-     * @param  {Number} topNumber  The number to get the factorial value for
-     * @return {Number}            The final factorial value
-     */
-    function factorial(topNumber) {
-        var i;
-        var total = 0;
-
-        if (typeof(topNumber) !== 'number') {
-            return 0;
-        }
-
-        for (i=1; i<=topNumber; i++) {
-            total *= i;
-        }
+    if (!numbers) {
         return total;
     }
 
-})();
+    numbers.forEach(function addThem(num) {
+        total += num;
+    });
+    return total
+}
+
+
+/**
+ * Returns the factorial for the given number which is defined as:
+ *    1 * 2 * 3 * ... * topNumber
+ *    For example, if the number is 5, the factorial would be:
+ *    1 * 2 * 3 * 4 * 5 = 120
+ * @param  {Number} topNumber  The number to get the factorial value for
+ * @return {Number}            The final factorial value
+ */
+module.exports.factorial = function factorial(topNumber) {
+    var i;
+    var total = 1;
+
+    if (typeof(topNumber) !== 'number') {
+        return 0;
+    }
+
+    for (i = 1; i <= topNumber; i++) {
+        total *= i;
+    }
+    return total;
+}
+
+
+// })();
--- test/specs/calc.js
+++ test/specs/calc.js
@@ -1,9 +1,31 @@
-(function() {
-    'use strict';
+// TODO: Your tests go here!
+
+let assert = require('assert')
+let deepEqual = require('../../../../Testing/node_modules/chai').deepEqual
+let sum = require('../../src/js/calc').sum
+let factorial = require('../../src/js/calc').factorial
+
+describe('sum', function() {
+    it('should give sum of array values', function() {
+        let a = [1, 2, 3]
+        let b = sum(a)
+        assert.equal(b, 6)
+    })
+})
+
+describe('factorial', function() {
+    it('should give factor of value number', function() {
+        let total = factorial(5)
+        assert.deepEqual(total, 120)
+    })
+})
 
 
-    // TODO: Your tests go here!
 
 
-
-})();
+//
+//
+//     (function() {
+//         'use strict';
+//
+// })();
